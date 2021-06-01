1e95eba74fb8a1c32742997e6c5ddbcd3fff8bb4
Murzin Andrey <MVMurzin@sberbank.ru>
2021-06-01 22:50:43 +0300

    add bottom bar

--- app/src/main/java/andrey/murzin/com/personcapital/ui/PersonCapitalApp.kt
+++ app/src/main/java/andrey/murzin/com/personcapital/ui/PersonCapitalApp.kt
@@ -1,14 +1,38 @@
 package andrey.murzin.com.personcapital.ui
 
+import andrey.murzin.com.personcapital.PersonNavGraph
+import andrey.murzin.com.personcapital.ui.home.HomeSections
+import andrey.murzin.com.personcapital.ui.home.PersonCapitalBottomBar
 import andrey.murzin.com.personcapital.ui.theme.PersonCapitalTheme
+import android.util.Log
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
 import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.insets.statusBarsPadding
 
 @Composable
 fun PersonCapitalApp() {
     ProvideWindowInsets {
         PersonCapitalTheme {
+            val tabs = remember { HomeSections.values().toList() }
+            val navController = rememberNavController()
 
+            Scaffold(
+                bottomBar = {
+                    PersonCapitalBottomBar(tabs = tabs, navController = navController)
+                }
+            ) { paddingValue ->
+                Log.d("PersonCapitalApp", "paddingValue = $paddingValue")
+                PersonNavGraph(
+                    navController = navController,
+                    startDestination = tabs.first().route,
+                    modifier = Modifier.padding(paddingValue).statusBarsPadding(),
+                )
+            }
         }
     }
 }
---
+++ app/src/main/java/andrey/murzin/com/personcapital/ui/oprationhistory/OperationHistory.kt
@@ -0,0 +1,18 @@
+package andrey.murzin.com.personcapital.ui.oprationhistory
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun OperationHistory(modifier: Modifier) {
+    Box(modifier = modifier.fillMaxSize()) {
+        Text(text = "OperationHistory")
+    }
+}
